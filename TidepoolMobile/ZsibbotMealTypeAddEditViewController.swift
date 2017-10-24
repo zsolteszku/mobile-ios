f01a20a68afe4dbf911c7ff4004a2d7af7d55392
Zsolt Essig <zsolteszku@gmail.com>
2017-10-23 23:36:13 -0700

    removeHashTagScrollView

--- TidepoolMobile/ZsibbotMealTypeAddEditViewController.swift
+++ TidepoolMobile/ZsibbotMealTypeAddEditViewController.swift
@@ -33,6 +33,37 @@ class ZsibbotMealTypeAddEditViewController: EventAddEditViewController {
     override func configureTitleView() {
         self.navItem.title = isAddNote ? "Add Meal Type" : "Edit Meal Type"
     }
+    
+    func removeHashTagScrollView() {
+        // remove hashTagScrollView
+        self.sceneContainerView.willRemoveSubview(self.hashtagsScrollView)
+        self.hashtagsScrollView.removeFromSuperview()
+        
+        // remove separator two
+        self.sceneContainerView.willRemoveSubview(self.separatorTwo)
+        self.separatorTwo.removeFromSuperview()
+        
+        
+    }
+    
+    func recalcMessageBox(lastSubView: UIView) {
+        let messageBoxW = self.sceneContainerView.frame.width - 2 * labelInset
+        let messageBoxH = (postButtonFrame.minY - lastSubView.frame.maxY) - 2 * labelInset
+        messageBox.frame.size = CGSize(width: messageBoxW, height: messageBoxH)
+        
+        messageBox.frame.origin.x = labelInset
+        messageBox.frame.origin.y = lastSubView.frame.maxY + labelInset
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        removeHashTagScrollView()
+        
+        recalcMessageBox(lastSubView: separatorOne)
+        
+        messageBox.isEditable = false
+    }
 }
 
 
